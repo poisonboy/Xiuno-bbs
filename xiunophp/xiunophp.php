db37274f7a416bc871a2e1c3f5e8b4393fe746e3
twelve <468909765>
2024-07-07 15:52:49 +0800

    Update xiunophp.php

--- xiunophp/xiunophp.php
+++ xiunophp/xiunophp.php
@@ -10,19 +10,19 @@
     5. 尽量采用函数封装功能，通过前缀区分模块
 */
 
-!defined('DEBUG') and define('DEBUG', 1); // 1: 开发模式， 2: 线上调试：日志记录，0: 关闭
-!defined('APP_PATH') and define('APP_PATH', './');
-!defined('XIUNOPHP_PATH') and define('XIUNOPHP_PATH', dirname(__FILE__) . '/');
+!defined('DEBUG') AND define('DEBUG', 1); // 1: 开发模式， 2: 线上调试：日志记录，0: 关闭
+!defined('APP_PATH') AND define('APP_PATH', './');
+!defined('XIUNOPHP_PATH') AND define('XIUNOPHP_PATH', dirname(__FILE__).'/');
 
-function_exists('ini_set') and ini_set('display_errors', DEBUG ? '1' : '0');
+function_exists('ini_set') AND ini_set('display_errors', DEBUG ? '1' : '0');
 error_reporting(DEBUG ? E_ALL : 0);
 
 // 在 PHP 5.4 及以上版本中，不再需要 set_magic_quotes_runtime 和 get_magic_quotes_gpc
 if (version_compare(PHP_VERSION, '5.4.0', '<')) {
-	set_magic_quotes_runtime(0);
-	$get_magic_quotes_gpc = get_magic_quotes_gpc();
+    set_magic_quotes_runtime(0);
+    $get_magic_quotes_gpc = get_magic_quotes_gpc();
 } else {
-	$get_magic_quotes_gpc = false;
+    $get_magic_quotes_gpc = false;
 }
 
 $starttime = microtime(1);
@@ -31,52 +31,52 @@ $time = time();
 // 头部，判断是否运行在命令行下
 define('IN_CMD', PHP_SAPI === 'cli');
 if (IN_CMD) {
-	!isset($_SERVER['REMOTE_ADDR']) and $_SERVER['REMOTE_ADDR'] = '';
-	!isset($_SERVER['REQUEST_URI']) and $_SERVER['REQUEST_URI'] = '';
-	!isset($_SERVER['REQUEST_METHOD']) and $_SERVER['REQUEST_METHOD'] = 'GET';
+    !isset($_SERVER['REMOTE_ADDR']) AND $_SERVER['REMOTE_ADDR'] = '';
+    !isset($_SERVER['REQUEST_URI']) AND $_SERVER['REQUEST_URI'] = '';
+    !isset($_SERVER['REQUEST_METHOD']) AND $_SERVER['REQUEST_METHOD'] = 'GET';
 } else {
-	header("Content-type: text/html; charset=utf-8");
-	//header("Cache-Control: max-age=0;"); // 手机返回的时候回导致刷新
-	//header("Cache-Control: no-store;");
-	//header("X-Powered-By: XiunoPHP 4.0");
+    header("Content-type: text/html; charset=utf-8");
+    //header("Cache-Control: max-age=0;"); // 手机返回的时候回导致刷新
+    //header("Cache-Control: no-store;");
+    //header("X-Powered-By: XiunoPHP 4.0");
 }
 
 // hook xiunophp_include_before.php
 
 // ----------------------------------------------------------> db cache class
 
-include XIUNOPHP_PATH . 'db_mysql.class.php';
-include XIUNOPHP_PATH . 'db_pdo_mysql.class.php';
-include XIUNOPHP_PATH . 'db_pdo_sqlite.class.php';
-include XIUNOPHP_PATH . 'cache_apc.class.php';
-include XIUNOPHP_PATH . 'cache_memcached.class.php';
-include XIUNOPHP_PATH . 'cache_mysql.class.php';
-include XIUNOPHP_PATH . 'cache_redis.class.php';
-include XIUNOPHP_PATH . 'cache_xcache.class.php';
-include XIUNOPHP_PATH . 'cache_yac.class.php';
+include XIUNOPHP_PATH.'db_mysql.class.php';
+include XIUNOPHP_PATH.'db_pdo_mysql.class.php';
+include XIUNOPHP_PATH.'db_pdo_sqlite.class.php';
+include XIUNOPHP_PATH.'cache_apc.class.php';
+include XIUNOPHP_PATH.'cache_memcached.class.php';
+include XIUNOPHP_PATH.'cache_mysql.class.php';
+include XIUNOPHP_PATH.'cache_redis.class.php';
+include XIUNOPHP_PATH.'cache_xcache.class.php';
+include XIUNOPHP_PATH.'cache_yac.class.php';
 
 // ----------------------------------------------------------> 全局函数
 
-include XIUNOPHP_PATH . 'db.func.php';
-include XIUNOPHP_PATH . 'cache.func.php';
-include XIUNOPHP_PATH . 'image.func.php';
-include XIUNOPHP_PATH . 'array.func.php';
-include XIUNOPHP_PATH . 'xn_encrypt.func.php';
-include XIUNOPHP_PATH . 'misc.func.php';
+include XIUNOPHP_PATH.'db.func.php';
+include XIUNOPHP_PATH.'cache.func.php';
+include XIUNOPHP_PATH.'image.func.php';
+include XIUNOPHP_PATH.'array.func.php';
+include XIUNOPHP_PATH.'xn_encrypt.func.php';
+include XIUNOPHP_PATH.'misc.func.php';
 
 // hook xiunophp_include_after.php
 
-empty($conf) and $conf = array('db' => array(), 'cache' => array(), 'tmp_path' => './', 'log_path' => './', 'timezone' => 'Asia/Shanghai');
-empty($conf['tmp_path']) and $conf['tmp_path'] = ini_get('upload_tmp_dir');
-empty($conf['log_path']) and $conf['log_path'] = './';
+empty($conf) AND $conf = array('db'=>array(), 'cache'=>array(), 'tmp_path'=>'./', 'log_path'=>'./', 'timezone'=>'Asia/Shanghai');
+empty($conf['tmp_path']) AND $conf['tmp_path'] = ini_get('upload_tmp_dir');
+empty($conf['log_path']) AND $conf['log_path'] = './';
 
 $ip = ip();
 $longip = ip2long($ip);
-$longip < 0 and $longip = sprintf("%u", $longip); // fix 32 位 OS 下溢出的问题
+$longip < 0 AND $longip = sprintf("%u", $longip); // fix 32 位 OS 下溢出的问题
 $useragent = $_SERVER['HTTP_USER_AGENT'] ?? '';
 
 // 语言包变量
-!isset($lang) and $lang = array();
+!isset($lang) AND $lang = array();
 
 // 全局的错误，非多线程下很方便。
 $errno = 0;
@@ -84,19 +84,19 @@ $errstr = '';
 
 // error_handle
 // register_shutdown_function('xn_shutdown_handle');
-DEBUG and set_error_handler('error_handle', -1);
-empty($conf['timezone']) and $conf['timezone'] = 'Asia/Shanghai';
+DEBUG AND set_error_handler('error_handle', -1);
+empty($conf['timezone']) AND $conf['timezone'] = 'Asia/Shanghai';
 date_default_timezone_set($conf['timezone']);
 
 // 超级全局变量
-!empty($_SERVER['HTTP_X_REWRITE_URL']) and $_SERVER['REQUEST_URI'] = $_SERVER['HTTP_X_REWRITE_URL'];
-!isset($_SERVER['REQUEST_URI']) and $_SERVER['REQUEST_URI'] = '';
+!empty($_SERVER['HTTP_X_REWRITE_URL']) AND $_SERVER['REQUEST_URI'] = $_SERVER['HTTP_X_REWRITE_URL'];
+!isset($_SERVER['REQUEST_URI']) AND $_SERVER['REQUEST_URI'] = '';
 $_SERVER['REQUEST_URI'] = str_replace('/index.php?', '/', $_SERVER['REQUEST_URI']); // 兼容 iis6
 $_REQUEST = array_merge($_COOKIE, $_POST, $_GET, xn_url_parse($_SERVER['REQUEST_URI']));
 
 // IP 地址
-!isset($_SERVER['REMOTE_ADDR']) and $_SERVER['REMOTE_ADDR'] = '';
-!isset($_SERVER['SERVER_ADDR']) and $_SERVER['SERVER_ADDR'] = '';
+!isset($_SERVER['REMOTE_ADDR']) AND $_SERVER['REMOTE_ADDR'] = '';
+!isset($_SERVER['SERVER_ADDR']) AND $_SERVER['SERVER_ADDR'] = '';
 
 // $_SERVER['REQUEST_METHOD'] === 'PUT' ? @parse_str(file_get_contents('php://input', false , null, -1 , $_SERVER['CONTENT_LENGTH']), $_PUT) : $_PUT = array(); // 不需要支持 PUT
 $ajax = (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower(trim($_SERVER['HTTP_X_REQUESTED_WITH'])) == 'xmlhttprequest') || param('ajax');
@@ -126,7 +126,9 @@ unset($conf['cache']['mysql']['db']); // 用完清除，防止保存到配置文
 //$cache AND $cache->errno AND xn_message(-1, $cache->errstr);
 
 // 对 key 进行安全保护，Xiuno 专用扩展
-!empty($conf) and (function_exists('xiuno_key') ? ($conf['auth_key'] = xiuno_key()) : NULL);
+!empty($conf) AND (function_exists('xiuno_key') ? ($conf['auth_key'] = xiuno_key()) : NULL);
 
 $_SERVER['db'] = $db;
 $_SERVER['cache'] = $cache;
+
+?>
